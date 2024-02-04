e2a66e1c6ecd23971d921e65b1e173cbe92fe599
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2024-02-04 22:45:47 +0530

    created MessageServer Class, created return_server_list method

--- Assignment-1/Part-2-ZeroMQ/message_server.py
+++ Assignment-1/Part-2-ZeroMQ/message_server.py
@@ -3,30 +3,52 @@ import zmq.asyncio
 
 context = zmq.asyncio.Context()
 
-serverList = dict()
 
-async def handle_socket():
-    socket = context.socket(zmq.REP)  # Change the socket type to REP
-    socket.bind("tcp://*:5000")  # Change the address as per your requirement
+class MessageServer:
+    """
+    Class to handle the message server
+    """
+    def __init__(self):
+        self.async_context: zmq.asyncio.Context = zmq.asyncio.Context()
+        self.serverList = dict()
 
-    while True:
-        message_parts = await socket.recv_multipart()
-        message = [part.decode() for part in message_parts]
+    async def handle_socket(self):
+        group_socket = context.socket(zmq.REP) 
+        group_socket.bind("tcp://*:5000")
 
-        print(f"LOG: Join Request from {message[0]} ({message[1]}:{message[2]})")
+        while True:
+            message_parts = await group_socket.recv_multipart()
+            message = [part.decode() for part in message_parts]
 
-        # check if group name already exists
-        if (message[0] in serverList):
-            await socket.send_string("FAILURE: Group name already exists")
+            print(f"LOG: Join Request from {message[0]} ({message[1]}:{message[2]})")
 
-        # check if ip address port number pair exists
-        elif (message[1] in serverList.values()):
-            await socket.send_string("FAILURE: IP address and port number already in use by another server")
+            # check if group name already exists
+            if (message[0] in self.serverList):
+                await group_socket.send_string("FAILURE: Group name already exists")
 
-        # add group to server list
-        else:
-            serverList[message[0]] = message[1]
-            await socket.send_string("SUCCESS")
-            print(f"SUCCESS: {message[0]} ({message[1]}) registered successfully")
+            # check if ip address port number pair exists
+            elif (message[1] in self.serverList.values()):
+                await group_socket.send_string("FAILURE: IP address and port number already in use by another server")
 
-asyncio.run(handle_socket())
+            # add group to server list
+            else:
+                self.serverList[message[0]] = message[1]
+                await group_socket.send_string("SUCCESS")
+                print(f"SUCCESS: {message[0]} ({message[1]}) registered successfully")
+
+    async def return_server_list(self):
+        """
+        Returns the server list
+        """
+        user_socket = context.socket(zmq.REP)
+        user_socket.bind("tcp://*:5001")
+
+        while True:
+            message_parts = await user_socket.recv_multipart()
+            message = [part.decode() for part in message_parts]
+
+            print(f"LOG: Request from {message[0]}")
+
+            # send the server list
+            await user_socket.send_json(self.serverList)
+        
