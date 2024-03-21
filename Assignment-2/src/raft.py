7cd697f76d9769c74f3549538dd5f970695edaa9
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2024-03-22 01:34:20 +0530

    fixed bugs in election

--- Assignment-2/src/raft.py
+++ Assignment-2/src/raft.py
@@ -32,11 +32,15 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
         self.last_applied:int = 0 # index of highest log entry applied to state machine (initialized to 0, increases monotonically)
 
         # leader specific volatile state, reinitialized after election
-        self.next_index:dict = dict()
+        self.next_index:dict = dict() # for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
         self.match_index:dict = dict()
 
         # leader id
         self.leader_id = None
+
+        # heatbeat timer
+        self.election_timeout = randint(5, 10)
+        self.curr_timeout = time()
   
     def leader_loop(self):
         # send AppendEntries RPCs to all peers
@@ -44,13 +48,14 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
         for id, peer in enumerate(NODES):
             if id == ID:
                 continue
+            print(f"Sending AppendEntries to {peer}")
             request:raft_pb2.AppendEntriesRequest = raft_pb2.AppendEntriesRequest()
             request.term = self.current_term
             request.leader_id = ID
-            request.prev_log_index = len(self.log) - 1
+            request.prev_log_idx = len(self.log) - 1
             request.prev_log_term = self.log[-1].term
             request.leader_commit_idx = self.commit_index
-            request.entries.extend(self.log[self.next_index[peer]:])
+            request.entries.extend(self.log[self.next_index.get(peer, 0):])
             response = stubs[peer].AppendEntries(request)
             if response.success:
                 self.next_index[peer] = len(self.log)
@@ -69,25 +74,27 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
                     self.commit_index = i
                     break
         
+        sleep(1)
+        
     def follower_loop(self):
         # maintain a timer for election timeout
-        election_timeout = randint(150, 300)
-        start_time = time()
-        while time() - start_time < election_timeout:
+        while (time() - self.curr_timeout) < self.election_timeout:
             pass
 
         # convert to candidate
+        print(f"Node {ID} converting to candidate")
         self.current_term += 1
         self.voted_for = ID
         votes = 1
         request:raft_pb2.VoteRequest = raft_pb2.VoteRequest()
         request.term = self.current_term
         request.candidate_id = ID
-        request.last_log_index = len(self.log) - 1
+        request.last_log_idx = len(self.log) - 1
         request.last_log_term = self.log[-1].term
         for id, peer in enumerate(NODES):
             if id == ID:
                 continue
+            print(f"Sending RequestVote to {peer}")
             response = stubs[peer].RequestVote(request)
             if response.vote_granted:
                 votes += 1
@@ -118,6 +125,7 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
 
     # RPC methods
     def RequestVote(self, request:raft_pb2.VoteRequest, context):
+        print(f"Received RequestVote from {request.candidate_id}")
         response:raft_pb2.VoteResponse = raft_pb2.VoteResponse()
 
         # if request.term < current_term, return false
@@ -127,7 +135,7 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
             return response
         
         # if voted_for is None or candidate_id, and candidate's log is at least as up-to-date as receiver's log, grant vote
-        if (self.voted_for is None or self.voted_for == request.candidate_id) and self.is_up_to_date(request.last_log_term, request.last_log_index):
+        if (self.voted_for is None or self.voted_for == request.candidate_id) and self.is_up_to_date(request.last_log_term, request.last_log_idx):
             self.voted_for = request.candidate_id
             response.term = self.current_term
             response.vote_granted = True
@@ -138,6 +146,7 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
         return response
 
     def AppendEntries(self, request:raft_pb2.AppendEntriesRequest, context):
+        print(f"Received AppendEntries from {request.leader_id}")
         response:raft_pb2.AppendEntriesResponse = raft_pb2.AppendEntriesResponse()
 
         if request.term < self.current_term:
@@ -145,15 +154,19 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
             response.success = False
             return response
 
-        # if log doesn't contain an entry at prev_log_index whose term matches prev_log_term
-        if len(self.log) < request.prev_log_index or self.log[request.prev_log_index].term != request.prev_log_term:
+        # if log doesn't contain an entry at prev_log_idx whose term matches prev_log_term
+        if len(self.log) < request.prev_log_idx or self.log[request.prev_log_idx].term != request.prev_log_term:
             response.term = self.current_term
             response.success = False
             return response
 
+        # refresh heartbeat timer
+        self.curr_timeout = time()
+
+
         # if an existing entry conflicts with a new one (same index but different terms)
-        if len(self.log) > request.prev_log_index and self.log[request.prev_log_index].term != request.prev_log_term:
-            self.log = self.log[:request.prev_log_index]
+        if len(self.log) > request.prev_log_idx and self.log[request.prev_log_idx].term != request.prev_log_term:
+            self.log = self.log[:request.prev_log_idx]
 
         # append any new entries not already in the log
         self.log.extend(request.entries)
@@ -206,10 +219,10 @@ if __name__ == "__main__":
     server.add_insecure_port(f"[::]:{DB_PORT}")
     try:
         server.start()
-        sleep(5)
+        sleep(1)
         # create stubs for all peers
         for node in NODES:
-            channel = grpc.insecure_channel(node + RAFT_PORT)
+            channel = grpc.insecure_channel(f"{node}:{RAFT_PORT}")
             stub = raft_pb2_grpc.RaftStub(channel)
             stubs[node] = stub
         raft_servicer.run()
