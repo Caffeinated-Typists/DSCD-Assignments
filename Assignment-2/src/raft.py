75a7ac9d852622f96fee5aff570e32949a5ac852
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-03-22 00:10:40 +0530

    implement some election steps

--- Assignment-2/src/raft.py
+++ Assignment-2/src/raft.py
@@ -1,14 +1,21 @@
+import os
 import json
 import grpc
 from concurrent import futures
 from random import randint
 from time import time
 from time import sleep
+import threading
+from urllib.parse import unquote
 
 import raft_pb2
 import raft_pb2_grpc
 
-nodes = ["10.0.1.1", "10.0.1.2", "10.0.1.3", "10.0.1.4", "10.0.1.5"]
+ID = int(os.environ["ID"])
+NODES = os.environ["NODES"].split(',')
+RAFT_PORT = os.environ["RAFT_PORT"]
+DB_PORT = os.environ["DB_PORT"]
+
 stubs = dict()
 
 class RaftServicer(raft_pb2_grpc.RaftServicer):
@@ -33,7 +40,6 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
     def run(self):
         self.set_election_timeout()
 
-
     def set_election_timeout(self, timeout=None):
         if timeout:
             self.election_timeout = timeout
@@ -53,12 +59,55 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
         self.votes.add(self.server_idx)
 
         # TODO: send RequestVote RPC to everyone
+        def make_rpc_call(stub):
+            request = raft_pb2.VoteRequest()
+            request.term = self.term
+            request.candidate_id = ID
+            request.last_log_idx = -1
+            request.last_log_term = -1
+
+            while True:
+                if self.state == 'CANDIDATE' and time() < self.election_timeout:
+                    response = stub.RequestVote(request)
+
+        threads = list()
+        for node,stub in stubs.items():
+            if node != NODES[ID]:
+                threads.append(threading.Thread(target=make_rpc_call, args=(stub,)))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.stop()
         
         return True
     
+    def step_down(self, term):
+        self.term = term
+        self.state = 'FOLLOWER'
+        self.voted_for = -1
+        self.set_election_timeout()
+    
     # RPC methods
     def RequestVote(self, request, context):
-        return super().RequestVote(request, context)
+        node = unquote(context.peer())
+        node_id = -1 # node # TODO: translate from node ip to id
+        if request.term > self.term:
+            self.step_down(request.term)
+
+        self.last_log_term = -1 
+        self.last_log_idx = -1 
+
+        if request.term == self.term \
+            and (self.voted_for == node_id or self.voted_for == -1) \
+            and (request.last_log_term == self.last_log_term and request.last_log_idx >= self.last_log_idx):
+                self.voted_for = node_id
+                self.state = 'FOLLOWER'
+                self.set_election_timeout()
+        
+        response = raft_pb2.VoteResponse()
+        response.term = self.term
+        response.vote_granted = True
+        return response
 
     def AppendEntries(self, request, context):
         return super().AppendEntries(request, context)
@@ -68,12 +117,12 @@ class RaftServicer(raft_pb2_grpc.RaftServicer):
 
 class DatabaseServicer(raft_pb2_grpc.DatabaseServicer):
     def __init__(self):
-        self.data:dict
+        self.data:dict = dict()
         try:
             with open("db.json", "r") as f:
                 self.data = json.load(f)
         except FileNotFoundError:
-            self.data[str:str] = dict()
+            self.data = dict()
         except Exception as e:
             raise e
 
@@ -98,14 +147,17 @@ class DatabaseServicer(raft_pb2_grpc.DatabaseServicer):
 if __name__ == "__main__":
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     raft_servicer = RaftServicer()
+    db_servicer = DatabaseServicer()
     raft_pb2_grpc.add_RaftServicer_to_server(raft_servicer, server)
-    server.add_insecure_port(f"[::]:{4444}")
+    raft_pb2_grpc.add_DatabaseServicer_to_server(db_servicer, server)
+    server.add_insecure_port(f"[::]:{RAFT_PORT}")
+    server.add_insecure_port(f"[::]:{DB_PORT}")
     try:
         server.start()
-        sleep(15)
+        sleep(5)
         # create stubs for all peers
-        for node in nodes:
-            channel = grpc.insecure_channel(node + ":4444")
+        for node in NODES:
+            channel = grpc.insecure_channel(node + RAFT_PORT)
             stub = raft_pb2_grpc.RaftStub(channel)
             stubs[node] = stub
         raft_servicer.run()
