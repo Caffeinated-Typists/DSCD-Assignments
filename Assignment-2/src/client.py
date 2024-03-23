c32582b57f8efc26328f45d5afebc5244ff995c7
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-03-24 00:16:56 +0530

    refactor client

--- Assignment-2/src/client.py
+++ Assignment-2/src/client.py
@@ -1,6 +1,6 @@
 import os
+import sys
 import grpc
-from random import randint
 
 import raft_pb2
 import raft_pb2_grpc
@@ -8,19 +8,10 @@ import raft_pb2_grpc
 NODES = os.environ["NODES"].split(',')
 RAFT_PORT = os.environ["RAFT_PORT"]
 
-stubs = dict()
-
 if __name__ == "__main__":
-    try:
-        # create stubs for all peers
-        for node in NODES:
-            channel = grpc.insecure_channel(f"{node}:{RAFT_PORT}")
-            stub = raft_pb2_grpc.RaftStub(channel)
-            stubs[node] = stub
-    except:
-        print("failed to create stubs")
-
-    stub = [x for x in stubs.values()][randint(0, len(NODES))]
+    node = NODES[int(sys.argv[1])]
+    channel = grpc.insecure_channel(f"{node}:{RAFT_PORT}")
+    stub = raft_pb2_grpc.RaftStub(channel)
 
     request = raft_pb2.DataRequest()
     request.data.cmd = raft_pb2.Log.SET
