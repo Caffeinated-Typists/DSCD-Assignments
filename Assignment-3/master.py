1d68d6d3b53213f6a454e8e9ee5f01beb1a03bea
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-04-17 12:56:01 +0530

    move mapper-reducer start/stop logic into seperate functions

--- Assignment-3/master.py
+++ Assignment-3/master.py
@@ -28,19 +28,33 @@ class MasterServicer(mapreduce_pb2_grpc.MasterServicer):
     def ReduceDone(self, request, context):
         return super().ReduceDone(request, context)
 
-    def run(self, args):
-        logging.info(f"Starting {args.num_mappers} mapper instances.")
-        for mapper_id in range(BASE_PORT_MAPPER, BASE_PORT_MAPPER + args.num_mappers):
+    def start_mappers(self, num_mappers):
+        logging.info(f"Starting {num_mappers} mapper instances.")
+        for mapper_id in range(BASE_PORT_MAPPER, BASE_PORT_MAPPER + num_mappers):
             self.mapper_proc[mapper_id] = Popen(["python3", "mapper.py", str(mapper_id)])
             channel = grpc.insecure_channel(f"127.0.0.1:{mapper_id}")
             self.mapper[mapper_id] = mapreduce_pb2_grpc.MapperStub(channel)
 
-        logging.info(f"Starting {args.num_reducers} recducer instances.")
-        for reducer_id in range(BASE_PORT_REDUCER, BASE_PORT_REDUCER + args.num_reducers):
+    def start_reducers(self, num_reducers):
+        logging.info(f"Starting {num_reducers} recducer instances.")
+        for reducer_id in range(BASE_PORT_REDUCER, BASE_PORT_REDUCER + num_reducers):
             self.reducer_proc[reducer_id] = Popen(["python3", "reducer.py", str(reducer_id)])
             channel = grpc.insecure_channel(f"127.0.0.1:{reducer_id}")
             self.reducer[reducer_id] = mapreduce_pb2_grpc.ReducerStub(channel)
 
+    def stop_mappers(self):
+        logging.info(f"Stopping {len(self.mapper_proc)} mapper instances.")
+        for mapper in self.mapper_proc.values():
+            mapper.terminate()
+
+    def stop_reducers(self):
+        logging.info(f"Stopping {len(self.reducer_proc)} reducer instances.")
+        for reducer in self.reducer_proc.values():
+            reducer.terminate()
+
+    def run(self, args):
+        self.start_mappers(args.num_mappers)
+        self.start_reducers(args.num_reducers)
         sleep(2) # wait for mappers and reducers to start
 
         logging.info(f"Reading input data from '{POINTS_FILE}'.")
@@ -57,14 +71,6 @@ class MasterServicer(mapreduce_pb2_grpc.MasterServicer):
             req.start, req.end = distrib[i]
             req.reducers = args.num_reducers
             res = self.mapper[mid].Map(req)
-
-        logging.info(f"Stopping {args.num_mappers} mapper instances.")
-        for mapper in self.mapper_proc.values():
-            mapper.terminate()
-        
-        logging.info(f"Stopping {args.num_reducers} reducer instances.")
-        for reducer in self.reducer_proc.values():
-            reducer.terminate()
 
 
 if __name__ == "__main__":
@@ -90,4 +96,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         logging.info("KeyboardInterrupt received.")
         server.stop(grace=None)
+    master.stop_mappers()
+    master.stop_reducers()
     logging.info("Master stopped.")
