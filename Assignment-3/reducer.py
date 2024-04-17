dfa076e48163af1859c35ddd4f2fe6c56e8bca1b
Aakarsh Jain <jainaakarsh09@gmail.com>
2024-04-17 17:59:12 +0530

    Added reducer functionality

--- Assignment-3/reducer.py
+++ Assignment-3/reducer.py
@@ -11,43 +11,79 @@ import mapreduce_pb2
 import mapreduce_pb2_grpc
 
 MASTER_PORT:int = 50000
+BASE_PORT_MAPPER: int = 50100
 
 CENTRIODS_FILE:str = "Data/centroids.txt"
 
 class ReducerServicer(mapreduce_pb2_grpc.ReducerServicer):
 
-    def __init__(self):
+    def __init__(self)->None:
 
+        self.num_dims = None
         self.reducer_id = None
         self.partition_idx = None
         self.num_mappers = None
         self.centroid_points = None
 
-    def reduce_compute(self):
-        
-        # Calculate the new centroid for the partition
-        #   - handle edge case where all points do not belong to the same centroid
-        self.centroid_id = None
-        self.accumulated_points = None
-        self.point_cnt = None
+    def __assert_dimensions(self, points:np.ndarray)->None:
+            
+            if self.num_dims is None:
+                self.num_dims = points.shape[1]
+            else:
+                assert self.num_dims == points.shape[1], "All points must have the same number of dimensions"
 
+    def reduce_compute(self)->None:
+
+        self.centroid_update = dict()
+        for centroid_id in self.centroid_points.keys():
+            self.centroid_update[centroid_id] = [np.zeros(self.num_dims), 0]
+
+        # Calculate the new centroid for the partition
+        for centroid_id in self.centroid_points.keys():
+            for point in self.centroid_points[centroid_id]:
+                self.centroid_update[centroid_id][0] += point
+                self.centroid_update[centroid_id][1] += 1
+
+        self.new_centroids = dict()
+        for centroid_id in self.centroid_update.keys():
+            self.new_centroids[centroid_id] = self.centroid_update[centroid_id][0] / self.centroid_update[centroid_id][1]
 
         # Send the new centroid to the master
         resp:mapreduce_pb2.ReduceResult = mapreduce_pb2.ReduceResult()
         resp.status = True
-        resp.centroid_id = self.centroid_id
-        resp.updated_point = self.accumulated_points/self.point_cnt
+        resp.data = pickle.dumps(self.new_centroids)
         master_stub.ReduceDone()
 
-        pass
+        return
 
-    def Reduce(self, request:mapreduce_pb2.ReduceRequest, context):
+    def Reduce(self, request:mapreduce_pb2.ReduceRequest, context)->mapreduce_pb2.Response:
 
         self.reducer_id = request.id
         self.partition_idx = request.partition_idx
         self.num_mappers = request.mappers
 
         # Fetch the points from the mappers and store them in self.centroid_points
+        #   - Assert that all the points have the same number of dimensions
+
+        self.centroid_points = dict()
+
+        for mapper_port in range(BASE_PORT_MAPPER, BASE_PORT_MAPPER + self.num_mappers):
+            channel = grpc.insecure_channel(f"127.0.0.1:{mapper_port}")
+            mapper_stub = mapreduce_pb2_grpc.MapperStub(channel)
+
+            req:mapreduce_pb2.PartitionRequest = mapreduce_pb2.PartitionRequest(idx=self.partition_idx)
+            resp:mapreduce_pb2.PartitionResponse = mapper_stub.GetPartition(req)
+
+            res_data = pickle.loads(resp.data)
+
+            for centroid_id in res_data.keys():
+
+                for point in res_data[centroid_id]:
+                    self.__assert_dimensions(point)
+
+                if self.centroid_points.get(centroid_id) is None:
+                    self.centroid_points[centroid_id] = list()
+                self.centroid_points[centroid_id].extend(res_data[centroid_id])
 
         # Launch the reduce_compute method in a separate thread
         thread = threading.Thread(target=self.reduce_compute)
@@ -56,12 +92,9 @@ class ReducerServicer(mapreduce_pb2_grpc.ReducerServicer):
         return mapreduce_pb2.Response(status=True)
 
         
-    def Ping(self, request, context):
+    def Ping(self, request, context)->mapreduce_pb2.Response:
+        return mapreduce_pb2.Response(status=True)
 
-        response:mapreduce_pb2.Response = mapreduce_pb2.Response()
-        response.status = True
-
-        return response
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
