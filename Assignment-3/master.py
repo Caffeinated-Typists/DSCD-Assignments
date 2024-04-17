bc56ec368ee52e6e64196ed538bd16c8d4395dc7
Prateek Kumar <prateek21081@iiitd.ac.in>
2024-04-17 15:09:53 +0530

    refactor mapper-reducer state handling in master

--- Assignment-3/master.py
+++ Assignment-3/master.py
@@ -13,79 +13,83 @@ BASE_PORT_MAPPER: int = 50100
 BASE_PORT_REDUCER: int = 50200
 
 POINTS_FILE: str = "Data/Input/points.txt"
+CENTRIODS_FILE:str = "Data/centroids.txt"
+
+# custom classes to store data and state for a mapper/reducer
+class Mapper:
+    port: int # also considered as id
+    stub: mapreduce_pb2_grpc.MapperStub
+    process: Popen # mapper process obj
+    request: mapreduce_pb2.MapRequest
+    status: bool
+
+    def __init__(self, port: int) -> None:
+        self.port = port
+        self.request = mapreduce_pb2.MapRequest()
+        logging.info(f"Starting mapper instance {self.port}.")
+        self.process = Popen(["python3", "mapper.py", str(self.port)])
+        channel = grpc.insecure_channel(f"127.0.0.1:{self.port}")
+        self.stub = mapreduce_pb2_grpc.MapperStub(channel)
+
+    def __del__(self) -> None:
+        logging.info(f"Stopping mapper instance {self.port}.")
+        self.process.terminate()
+
+class Reducer:
+    port: int # alson considered as id
+    stub: mapreduce_pb2_grpc.ReducerStub
+    process: Popen # reducer process obj
+    status: bool
+
+    def __init__(self, port: int) -> None:
+        self.port = port
+        logging.info(f"Starting reducer instance {self.port}.")
+        self.process = Popen(["python3", "reducer.py", str(self.port)])
+        channel = grpc.insecure_channel(f"127.0.0.1:{self.port}")
+        self.stub = mapreduce_pb2_grpc.ReducerStub(channel)
+
+    def __del__(self) -> None:
+        logging.info(f"Stopping reducer instance {self.port}.")
+        self.process.terminate()
 
 class MasterServicer(mapreduce_pb2_grpc.MasterServicer):
-    num_mappers: int = 1
-    num_reducers: int = 1
-    # <instance id/port> : <intstance proc>
-    mapper_proc: dict[int, Popen] = dict() 
-    reducer_proc: dict[int, Popen] = dict()
-    # <instance id/port> : <intstance stub>
-    mapper: dict[int, mapreduce_pb2_grpc.MapperStub] = dict()
-    reducer: dict[int, mapreduce_pb2_grpc.ReducerStub] = dict()
+    num_points: int = 0
+    num_mappers: int = 0
+    num_reducers: int = 0
+    num_centroids: int = 0
+    num_iterations: int = 0
 
-    num_iterations: int = 1
-    
-    num_centroids: int = 1
+    mappers: dict[int, Mapper] = dict()
+    reducers: dict[int, Reducer] = dict()
     centroids: list[float] = list()
-
-    map_input_splits: list[tuple[int, int]] = list()
 
     def __init__(self, args) -> None:
         super().__init__()
         self.num_mappers = args.num_mappers
         self.num_reducers = args.num_reducers
-        self.num_iterations = args.num_iterations
         self.num_centroids = args.num_centroids
-
-    def start_mappers(self):
-        logging.info(f"Starting {self.num_mappers} mapper instances.")
-        for mapper_id in range(BASE_PORT_MAPPER, BASE_PORT_MAPPER + self.num_mappers):
-            self.mapper_proc[mapper_id] = Popen(["python3", "mapper.py", str(mapper_id)])
-            channel = grpc.insecure_channel(f"127.0.0.1:{mapper_id}")
-            self.mapper[mapper_id] = mapreduce_pb2_grpc.MapperStub(channel)
-
-    def start_reducers(self):
-        logging.info(f"Starting {self.num_reducers} recducer instances.")
-        for reducer_id in range(BASE_PORT_REDUCER, BASE_PORT_REDUCER + self.num_reducers):
-            self.reducer_proc[reducer_id] = Popen(["python3", "reducer.py", str(reducer_id)])
-            channel = grpc.insecure_channel(f"127.0.0.1:{reducer_id}")
-            self.reducer[reducer_id] = mapreduce_pb2_grpc.ReducerStub(channel)
-
-    def stop_mappers(self):
-        logging.info(f"Stopping {len(self.mapper_proc)} mapper instances.")
-        for mapper in self.mapper_proc.values():
-            mapper.terminate()
-
-    def stop_reducers(self):
-        logging.info(f"Stopping {len(self.reducer_proc)} reducer instances.")
-        for reducer in self.reducer_proc.values():
-            reducer.terminate()
-
-    def gen_map_input_splits(self):
+        self.num_iterations = args.num_iterations
         logging.info(f"Reading input data from '{POINTS_FILE}'.")
         with open(POINTS_FILE) as points_file:
-            points = points_file.readlines()
-            print(f"read {len(points)} points.")
-            q, r = divmod(len(points), args.num_mappers)
-            self.map_input_splits = [(i * q + min(i, r), (i + 1) * q + min(i+1, r)) for i in range(self.num_mappers)]
+            self.num_points = len(points_file.readlines())
+            logging.info(f"Read {self.num_points} points.")
 
     def run(self):
-        self.start_mappers()
-        self.start_reducers()
+        q, r = divmod(self.num_points, args.num_mappers)
+        for i in range(self.num_mappers):
+            id = BASE_PORT_MAPPER + i
+            mapper = Mapper(id)
+            mapper.request.id = id
+            mapper.request.reducers = self.num_reducers
+            mapper.request.start = i * q + min(i, r)
+            mapper.request.end = (i + 1) * q + min(i+1, r)
+            self.mappers[id] = mapper
+        
+        for i in range(self.num_reducers):
+            id = BASE_PORT_REDUCER + i
+            self.reducers[id] = Reducer(id)
+
         sleep(2) # wait for mappers and reducers to start
-
-        self.gen_map_input_splits()
-
-        # start map jobs
-        map_request = mapreduce_pb2.MapRequest()
-        map_request.reducers = self.num_reducers
-        map_request.centroids.extend(self.centroids)
-        for i, mapper_id in enumerate(self.mapper.keys()):
-            map_request.id = mapper_id
-            map_request.start, map_request.end = self.map_input_splits[i]
-            map_response = self.mapper[mapper_id].Map(map_request)
-            print(map_response)
 
     def MapDone(self, request, context):
         return super().MapDone(request, context)
@@ -117,6 +121,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         logging.info("KeyboardInterrupt received.")
         server.stop(grace=None)
-    master.stop_mappers()
-    master.stop_reducers()
     logging.info("Master stopped.")
